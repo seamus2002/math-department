984f00a16ab09204aa2e5efdb9933db763a4499b
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-04-12 12:42:38 -0400

    added image placeholder for faculty

--- src/components/FullTimeCard.tsx
+++ src/components/FullTimeCard.tsx
@@ -5,6 +5,8 @@ const FullTimeCard = (props: any) => {
 		<div>
 			<div className="card mx-auto">
 				<div className="card-body">
+					<svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+					{/* <img src="..." className="card-img-top" alt="..." /> */}
 					<h5 className="card-title">{props.name}</h5>
 					<h6 className="card-subtitle mb-2 text-muted">{props.position}</h6>
 					<h6 className="card-subtitle mb-2">Interests: {props.interests}</h6>
