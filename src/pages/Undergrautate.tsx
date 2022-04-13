85d7c850b603e36d264784dd73e097eb3321f5f0
seamus2002 <seamus.english58@myhunter.cuny.edu>
2022-04-12 21:39:56 -0400

    added bama section

--- src/pages/Undergrautate.tsx
+++ src/pages/Undergrautate.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import UMath from './subpages/UMath';
 import UStats from './subpages/UStats';
 import ULogic from './subpages/ULogic';
+import UBAMA from './subpages/UBAMA';
 
 const Undergrautate = () => {
     const [page, setPage] = useState(<UMath />);
@@ -15,6 +16,7 @@ const Undergrautate = () => {
             <button type="button" className="btn btn-info faculty-btn" onClick={() => {setPage(<UMath />)}}>Mathematics</button>
             <button type="button" className="btn btn-info faculty-btn" onClick={() => {setPage(<UStats />)}}>Statistics</button>
             <button type="button" className="btn btn-info faculty-btn" onClick={() => {setPage(<ULogic />)}}>Logic</button>
+            <button type="button" className="btn btn-info faculty-btn" onClick={() => {setPage(<UBAMA />)}}>BA/MA Programs</button>
             {page}
         </div>
     );
