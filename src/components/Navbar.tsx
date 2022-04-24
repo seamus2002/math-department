2a77c3f71268818a760fb052bdc489c706251098
seamus2002 <seamus.english58@myhunter.cuny.edu>
2022-04-24 18:50:04 -0400

    upgraded to React Router v6

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 
 // Pages
 import Home from '../pages/Home';
@@ -10,39 +10,29 @@ import Undergrautate from '../pages/Undergrautate';
 const Navbar = () => {
   return (
     <div>
-        <BrowserRouter>
-            <nav className="navbar fixed-top navbar-expand-lg navbar-dark" style={{backgroundColor: '#5f259f'}}>
-                <div className="container-fluid">
-                    <NavLink exact to='/' className="navbar-brand d-none d-lg-block">Department of Mathematics and Statistics</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item"><NavLink exact to='/' className='nav-link'>Home</NavLink></li>
-                            <li className="nav-item"><NavLink to='/people' className='nav-link'>People</NavLink></li>
-                            <li className="nav-item"><NavLink to='/undergraduate' className='nav-link'>Undergraduate</NavLink></li>
-                            <li className="nav-item"><NavLink to='/courses' className='nav-link'>Courses</NavLink></li>
-                        </ul>
-                    </div>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark" style={{backgroundColor: '#5f259f'}}>
+            <div className="container-fluid">
+                <NavLink to='/' className="navbar-brand d-none d-lg-block">Department of Mathematics and Statistics</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><NavLink to='/' className='nav-link'>Home</NavLink></li>
+                        <li className="nav-item"><NavLink to='/people' className='nav-link'>People</NavLink></li>
+                        <li className="nav-item"><NavLink to='/undergraduate' className='nav-link'>Undergraduate</NavLink></li>
+                        <li className="nav-item"><NavLink to='/courses' className='nav-link'>Courses</NavLink></li>
+                    </ul>
                 </div>
-            </nav>
+            </div>
+        </nav>
 
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="/people">
-                    <People />
-                </Route>
-                <Route path="/undergraduate">
-                    <Undergrautate />
-                </Route>
-                <Route path="/courses">
-                    <Courses />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/people" element={<People />} />
+            <Route path="/undergraduate" element={<Undergrautate />} />
+            <Route path="/courses" element={<Courses />} />
+        </Routes>
     </div>
   );
 }
