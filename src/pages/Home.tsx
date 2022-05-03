eb88eace5bd4136414d013e6f05030e70bacde37
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-05-03 09:36:20 -0400

    add announcements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,19 @@ const Home = () => {
   return (
     <div className='container page'>
       <h1 className='page-header'>Welcome to the Department of Mathematics and Statistics!</h1>
-      <div className="announcements">
-        <h2>Announcements</h2>
-      </div>
+      <hr />
+      <h2>Announcements</h2>
+
+      <br />
+
+      <h5>May 4th, 2022</h5>
+      <p>The enrollment shopping cart opens today for the Fall 2022 term! Please check CUNYfirst for the course listings and your enrollment date.</p>
+
+      <br />
+      <br />
+      
+      <h5>May 3rd, 2022</h5>
+      <p>Welcome to the new website for the Department of Mathematics and Statistics! The site went through a major overhaul to be more visually pleasing and easier to navigate.</p>
     </div>
   );
 }
